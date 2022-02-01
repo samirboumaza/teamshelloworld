7cf0217a1e5f01dacd4272b05073feedd9a61446
Samir Boumaza <samir@bewave.io>
2022-02-01 14:50:35 -0500

    remove log

--- teams-hello-world/src/App.vue
+++ teams-hello-world/src/App.vue
@@ -59,7 +59,6 @@
 
     <!-- Sizes your content based upon application components -->
     <v-main>
-      {{ tabContext }}
       <!-- Provides the application the proper gutter -->
       <v-container fluid>
 
