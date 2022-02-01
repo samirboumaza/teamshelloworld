03bb5ea55b9f42a3458cc54d8bf5c27edcecb01f
Samir Boumaza <samir@bewave.io>
2022-02-01 13:36:28 -0500

    update style

--- teams-hello-world/src/App.vue
+++ teams-hello-world/src/App.vue
@@ -1,60 +1,115 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+    <v-container fluid class="fill-height pl-0" style="background-color: blue">
+    <v-card class="fill-height rounded-sm mt-13">
+      <v-navigation-drawer
+          permanent
+          expand-on-hover
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+        <v-list>
+          <v-list-item class="px-2">
+            <v-list-item-avatar>
+              <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+
+          <v-list-item link>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                Sandra Adams
+              </v-list-item-title>
+              <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list
+            nav
+            dense
+        >
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-folder</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>My Files</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-account-multiple</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Shared with me</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-star</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Starred</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+
+
+    <v-app-bar class='text--white text-h4 elevation-0' color="primary" app>
+      Test Teams app
     </v-app-bar>
 
+
+    <!-- Sizes your content based upon application components -->
     <v-main>
-      <HelloWorld/>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+      <v-card>
+        <v-card-title>
+          To do list
+        </v-card-title>
+        <v-card-subtitle>draft version</v-card-subtitle>
+
+        <v-card>
+          <v-card-actions></v-card-actions>
+        </v-card>
+
+      </v-card>
+      </v-container>
     </v-main>
-  </v-app>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-container></v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
+import * as microsoftTeams from "@microsoft/teams-js";
 
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  name: "App",
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+        { title: 'Account', icon: 'mdi-account-box' },
+        { title: 'Admin', icon: 'mdi-gavel' },
+      ],// Hide mobile side menu by default
+    };
   },
 
-  data: () => ({
-    //
-  }),
+  beforeCreate() {
+    microsoftTeams.initialize()
+  },
+  mounted() {
+    this.getContext()
+    },
+  methods:{
+    getContext(){
+      microsoftTeams.getContext((context)=> {
+      console.log("context :",context) })  }
+  }
 };
 </script>
---
+++ teams-hello-world/src/components/task.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "task.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
